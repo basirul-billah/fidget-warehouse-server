156b2ff9c7c4774a6e8a84e1d181c8c7e4505bdc
Basirul Billah <basirulbillah@gmail.com>
2022-05-11 09:51:10 +0600

    initial commit for server"

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const uri = "mongodb+srv://dbUser:<password>@cluster0.w0mx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
