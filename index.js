7ab5688a596b3809d716138a5fb63c587d62e2f8
Basirul Billah <basirulbillah@gmail.com>
2022-05-11 10:00:17 +0600

    added get and listening

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-const uri = "mongodb+srv://dbUser:<password>@cluster0.w0mx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// mongodb driver code 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0mx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(port, () => {
+    console.log('listening to port ', port);
+})
