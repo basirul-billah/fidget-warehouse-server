85b6b7f8a51bb9088801d77a7a1059bf9ab0107a
Basirul Billah <basirulbillah@gmail.com>
2022-05-12 11:47:32 +0600

    added api for single item

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,14 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb driver code 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0mx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0mx2.mongodb.net/fidgetWarehouseDB?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("fidgetWarehouse").collection("products");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db("fidgetWarehouse").collection("products");
+
+        // to load all products 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        // to load single product 
+        app.get('/itemdetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('hello world 2');
