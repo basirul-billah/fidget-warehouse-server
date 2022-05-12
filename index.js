15118e5b18baa711425dca77286004c8d87099e7
Basirul Billah <basirulbillah@gmail.com>
2022-05-12 08:56:12 +0600

    added middleware and mongodb driver code

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware 
+app.use(cors());
+app.use(express.json());
+
 // mongodb driver code 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w0mx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("fidgetWarehouse").collection("products");
   // perform actions on the collection object
   client.close();
 });
 
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world 2');
 })
 
 app.listen(port, () => {
-    console.log('listening to port ', port);
+    console.log('listening to port', port);
 })
